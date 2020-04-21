7c90e73d1b4dcf8f1103b2ff032cc877a6cf20f1
Ronald James Reyes <48842512+ronaldreyes224@users.noreply.github.com>
2020-04-21 13:10:56 -0700

    added some tests

--- src/test/java/buildcraft/test/lib/inventory/ItemTransactorTester.java
+++ src/test/java/buildcraft/test/lib/inventory/ItemTransactorTester.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.IInventory;
+import net.minecraftforge.common.ForgeDirection;
 
 import buildcraft.api.inventory.IItemTransactor;
 
@@ -14,6 +16,10 @@ import buildcraft.lib.tile.item.StackInsertionFunction;
 import buildcraft.test.VanillaSetupBaseTester;
 
 public class ItemTransactorTester extends VanillaSetupBaseTester {
+    private IInventory fakeInventory;
+    private ItemStack fakeStack;
+    private ItemStack injectStack;
+    
     @Test
     public void testSimpleMoving() {
         IItemTransactor trans = new ItemHandlerSimple(2, null);
@@ -46,5 +52,40 @@ public class ItemTransactorTester extends VanillaSetupBaseTester {
 
         Assert.assertTrue(ItemStack.areItemStacksEqual(toInsert, toInsertCopy));
         Assert.assertTrue(ItemStack.areItemStacksEqual(supposedLeftOver, actuallyLeftOver));
+    }
+    @Test
+    public void inject_inventoryHasNoSlots_returns0(){
+        when(fakeInventory.getSizeInventory()).thenReturn(0);
+
+        TransactorSimple cut = new TransactorSimple(fakeInventory);
+        int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, false);
+
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void inject_inventoryIsEmptyDontAdd_returns64(){
+        when(fakeInventory.getSizeInventory()).thenReturn(1);
+        when(fakeInventory.getStackInSlot(0)).thenReturn(null);
+        when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
+        injectStack.stackSize = 64;
+
+        TransactorSimple cut = new TransactorSimple(fakeInventory);
+        int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, false);
+
+        assertEquals(64, actual);
+    }
+
+    @Test
+    public void inject_inventoryIsEmptyDoAdd_returns64(){
+        when(fakeInventory.getSizeInventory()).thenReturn(1);
+        when(fakeInventory.getStackInSlot(0)).thenReturn(null);
+        when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
+        injectStack.stackSize = 64;
+
+        TransactorSimple cut = new TransactorSimple(fakeInventory);
+        int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, true);
+
+        assertEquals(64, actual);
     }
 }
