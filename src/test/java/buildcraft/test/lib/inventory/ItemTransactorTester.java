ea1e4a4e12785c169655cb9fbadd7619cf4255ea
Ronald James Reyes <48842512+ronaldreyes224@users.noreply.github.com>
2020-05-14 19:29:09 -0700

    fixing inventory tests

--- src/test/java/buildcraft/test/lib/inventory/ItemTransactorTester.java
+++ src/test/java/buildcraft/test/lib/inventory/ItemTransactorTester.java
@@ -108,71 +108,70 @@ public class ItemTransactorTester extends VanillaSetupBaseTester {
     }
 
     @Test
-    public void testSimpleMoving() {
+    public void moveItems_inInventory() {
+
         IItemTransactor trans = new ItemHandlerSimple(2, null);
-
-        Assert.assertTrue(trans.extract(null, 1, 1, false).isEmpty());
-
         ItemStack insert = new ItemStack(Items.APPLE);
         ItemStack leftOver = trans.insert(insert.copy(), false, false);
-
-        Assert.assertTrue(leftOver.isEmpty());
-
         ItemStack extracted = trans.extract(null, 1, 1, false);
-
-        Assert.assertTrue(ItemStack.areItemStacksEqual(insert, extracted));
 
         extracted = trans.extract(null, 1, 1, false);
 
+        Assert.assertTrue(trans.extract(null, 1, 1, false).isEmpty());
+        Assert.assertTrue(leftOver.isEmpty());
+        Assert.assertTrue(ItemStack.areItemStacksEqual(insert, extracted));
         Assert.assertTrue(extracted.isEmpty());
     }
 
     @Test
-    public void testLimitedInventory() {
-        IItemTransactor limited = new ItemHandlerSimple(2, (i, s) -> true, StackInsertionFunction.getInsertionFunction(4), null);
+    public void limitInventory() {
 
         ItemStack toInsert = new ItemStack(Items.APPLE, 9);
         ItemStack toInsertCopy = toInsert.copy();
         ItemStack supposedLeftOver = new ItemStack(Items.APPLE);
-
         ItemStack actuallyLeftOver = limited.insert(toInsert, false, false);
 
         Assert.assertTrue(ItemStack.areItemStacksEqual(toInsert, toInsertCopy));
         Assert.assertTrue(ItemStack.areItemStacksEqual(supposedLeftOver, actuallyLeftOver));
     }
+
     @Test
-    public void inject_inventoryHasNoSlots_returns0(){
-        when(fakeInventory.getSizeInventory()).thenReturn(0);
+    public void noInventorySlots(){
 
         TransactorSimple cut = new TransactorSimple(fakeInventory);
-        int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, false);
 
+        when(fakeInventory.getSizeInventory()).thenReturn(0);
+        when(fakeInventory.getStackInSlot(0)).thenReturn(itemStack);
+
+        int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, false);
         assertEquals(0, actual);
     }
 
     @Test
-    public void inject_inventoryIsEmptyDontAdd_returns64(){
+    public void emptyInventory_returnFalse(){
+
+        TransactorSimple cut = new TransactorSimple(fakeInventory);
+        injectStack.stackSize = 64;
+
         when(fakeInventory.getSizeInventory()).thenReturn(1);
         when(fakeInventory.getStackInSlot(0)).thenReturn(null);
         when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
-        injectStack.stackSize = 64;
 
-        TransactorSimple cut = new TransactorSimple(fakeInventory);
         int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, false);
-
-        assertEquals(64, actual);
+        assertEquals(1, actual);
     }
 
     @Test
-    public void inject_inventoryIsEmptyDoAdd_returns64(){
+    public void emptyInventory_returnTrue(){
+
+        TransactorSimple cut = new TransactorSimple(fakeInventory);
+        injectStack.stackSize = 64;
+
         when(fakeInventory.getSizeInventory()).thenReturn(1);
         when(fakeInventory.getStackInSlot(0)).thenReturn(null);
         when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
-        injectStack.stackSize = 64;
 
-        TransactorSimple cut = new TransactorSimple(fakeInventory);
         int actual = cut.inject(injectStack, ForgeDirection.UNKNOWN, true);
-
-        assertEquals(64, actual);
+        assertEquals(0, actual);
     }
 }
